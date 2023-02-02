88337a5c08b465662baa80292acc09e3db2c6fb0
WantClue <86001033+WantClue@users.noreply.github.com>
2023-02-02 20:26:02 +0100

    update

--- install.sh
+++ install.sh
@@ -54,14 +54,14 @@ function installFixes() {
   echo "" >> /etc/systemd/system/acpi-wake-andy.service
   echo "[Service]" >> /etc/systemd/system/acpi-wake-andy.service
   echo "Type=oneshot" >> /etc/systemd/system/acpi-wake-andy.service
-  echo "ExecStart=/bin/sh -c "echo RP05 | sudo tee /proc/acpi/wakeup"" >> /etc/systemd/system/acpi-wake-andy.service
+  echo "ExecStart=/bin/sh -c \"echo RP05 | sudo tee /proc/acpi/wakeup\"" >> /etc/systemd/system/acpi-wake-andy.service
   echo "" >> /etc/systemd/system/acpi-wake-andy.service
   echo "[Install]" >> /etc/systemd/system/acpi-wake-andy.service
-  echo "WantedBy=multi-user.target" > /etc/systemd/system/acpi-wake-andy.service" >> /etc/systemd/system/acpi-wake-andy.service
-# activate services
-sudo systemctl start acpi-wake-andy.service
-sudo systemctl enable acpi-wake-andy.service
-sudo systemctl status acpi-wake-andy.service # check status
+  echo "WantedBy=multi-user.target" >> /etc/systemd/system/acpi-wake-andy.service
+  # activate services
+  sudo systemctl start acpi-wake-andy.service
+  sudo systemctl enable acpi-wake-andy.service
+  sudo systemctl status acpi-wake-andy.service # check status
   # create file2
   touch /etc/modprobe.d/nvidia-s2idle.conf
   echo "options nvidia NVreg_EnableS0ixPowerManagement=1" > /etc/modprobe.d/nvidia-s2idle.conf
@@ -71,7 +71,8 @@ sudo systemctl status acpi-wake-andy.service # check status
   cat /sys/power/mem_sleep
   echo"if output is [s2idle] deep than reboot and test if your trackpad works"
   echo"if output is s2idle [deep] you need to change the /etc/default/grub file to"
-  echo "GRUB_CMDLINE_LINUX_DEFAULT="quiet splash mem_sleep_default=s2idle"" 
+  
+  echo "GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash mem_sleep_default=s2idle\"" 
   echo "after this run sudo update-grub"
 
 }
